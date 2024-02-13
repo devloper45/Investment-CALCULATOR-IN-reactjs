dc249d8589d2c02eff28b3bb401531a550f9b5f2
devloper45 <113242732+devloper45@users.noreply.github.com>
2024-02-13 19:01:26 +0500

    Add files via upload

---
+++ starter/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (<>
+    <h1>React Investment Calculator</h1>
+
+
+  </>
+
+  );
+}
+
+export default App;
+// initial investment 
+// annual investment
+// duration
+// expected return
+
+
+// components 
+// header user input  result
